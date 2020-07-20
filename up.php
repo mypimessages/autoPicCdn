f1780a476ff1e66c73182e8c23029e1fbfef3d8b
mypimessages <66762038+mypimessages@users.noreply.github.com>
2020-07-20 17:05:31 -0500

    Update up.php

--- up.php
+++ up.php
@@ -33,13 +33,13 @@ if(!is_callable('curl_init')){
 define("TYPE","GITHUB");//选择github
 //define("TYPE","GITEE");//选择gitee，如果使用gitee，需要手动建立master分支，可以看这里 https://gitee.com/help/articles/4122
 
-define("USER","pic-cdn");//你的GitHub/Gitee的用户名
+define("USER","mypimessages");//你的GitHub/Gitee的用户名
 
-define("REPO","cdn2");//必须是上面用户名下的 公开仓库
+define("REPO","cdn");//必须是上面用户名下的 公开仓库
 
-define("MAIL","yumusb@foxmail.com");//邮箱无所谓，随便写
+define("MAIL","mypimessages@outlook.com");//邮箱无所谓，随便写
 
-define("TOKEN","213");
+define("TOKEN","0e27d4f2a2d1b6390d60b89f5f42b71d09a88245");
 // Github 去这个页面 https://github.com/settings/tokens生成一个有写权限的token（repo：Full control of private repositories 和write:packages前打勾）
 // gitee  去往这个页面 https://gitee.com/personal_access_tokens
 
